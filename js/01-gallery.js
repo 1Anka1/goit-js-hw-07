97c562da855d4bc352ef089d62dc11c969762312
1Anka1 <97912793+1Anka1@users.noreply.github.com>
2022-07-16 23:51:41 +0200

    Task 1
    Add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallerycontainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+gallerycontainer.insertAdjacentHTML("beforeend", cardsMarkup);
+gallerycontainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+function onGalleryContainerClick(e) {
+  e.preventDefault();
+
+  const instance = basicLightbox.create(
+    `
+        <img src="${e.target.dataset.source}" width="800" height="600">
+    `
+  );
+  
+  instance.show();
+}
+// Тут невдалося реалізувати закриття Escape тому, що змінна instance не доступна
+// document.addEventListener("keydown", onEscKeyPress);
+// function onEscKeyPress(event) {
+//   if (event.code === 'Escape') {
+//     instance.close();
+//   }
+//   console.log(event.code);
+// }
